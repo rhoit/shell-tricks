5b1e94ebdcb50ed988aa2326e22fea0f527d9143
rho <rho.rhoit@gmail.com>
2014-12-28 22:55:57 +0545

    Bold the matrix stripe head
    * few mods and cleanup
    * hide cursor and echo

--- matrix.sh
+++ matrix.sh
@@ -7,29 +7,31 @@
 function strain_flow {
     x=$[RANDOM%COLUMNS] y=$[RANDOM%LINES]
     for ((i=1; i < $LINES; i++)); do
-        c=$(printf '\\0%o' $[RANDOM%26+97])
-        echo -e "\033[$[i-1];${x}H\033[32m$c\033[$i;${x}H\033[37m"$c
+        char=$(printf '\\0%o' $[RANDOM%26+97])
+        echo -e "\e[$[i-1];${x}H\e[0;32m$char\e[$i;${x}H\e[1;37m"$c
         sleep 0.1
         if (( $i > $y )); then
-            echo -e "\033[$[i-y];${x}H "
+            echo -e "\e[$[i-y];${x}H "
         fi
     done
 
     for ((i=i-y; i < $LINES; i++)); do
-        echo -ne "\033[$i;${x}f "
+        echo -ne "\e[$i;${x}f "
         sleep 0.1
     done
 }
 
 function neat_terminate {
-	tput cnorm # show cursor
-	stty echo # enable output
-	tput cup $LINES 0
+    tput cnorm # show cursor
+    stty echo # enable output
+    tput cup $LINES 0
     exit
 }
 
-# echo -e "\e[1;40m"
-echo -e "\033[2J\033[?25l"
+tput civis # hide cursor
+stty -echo # disable output
+clear
+
 LINES=$(tput lines)
 COLUMNS=$(tput cols)
 
